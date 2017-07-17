353489947c183cff1bda446b048ad336f632b144
jinuoA <1107194939@qq.com>
2017-07-17 16:59:29 +0800

    Update settings.py

--- XphoneProject/XphoneProject/settings.py
+++ XphoneProject/XphoneProject/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'XphoneProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb1',
+        'USER':'root',
+        'PASSWORD':'d14011b',
+        'HOST':'',
+        'POST':'',
     }
 }
 
